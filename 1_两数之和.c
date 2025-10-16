7c4780abbf7101fad304bb321dfba88b7fe9a529
王宇乐 <1125532674@qq.com>
2025-10-16 22:22:40 +0800

    Add files via upload

---
+++ 1_两数之和.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "uthash.h"
+
+struct hashTable {
+    int key;
+    int val;
+    UT_hash_handle hh;
+};
+//key存放数字，val存放下标
+
+struct hashTable* hashtable;    //哈希表
+
+//查找
+struct hashTable* find(int ikey) {
+    struct hashTable* tmp;
+    HASH_FIND_INT(hashtable, &ikey, tmp);
+    return tmp;
+}
+
+//插入
+void insert(int ikey, int ival) {
+    struct hashTable* it = find(ikey);
+    if (it == NULL) {
+        struct hashTable* tmp = malloc(sizeof(struct hashTable));
+        tmp->key = ikey, tmp->val = ival;
+        HASH_ADD_INT(hashtable, key, tmp);
+    } else {
+        it->val = ival;
+    }
+}
+
+
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    hashtable = NULL;
+    for (int i = 0; i < numsSize; i++) {
+        struct hashTable* it = find(target - nums[i]);
+        if (it != NULL) {
+            int* ret = malloc(sizeof(int) * 2);
+            ret[0] = it->val, ret[1] = i;
+            *returnSize = 2;
+            return ret;
+        }
+        insert(nums[i], i);
+    }
+    *returnSize = 0;
+    return NULL;
+}
+
