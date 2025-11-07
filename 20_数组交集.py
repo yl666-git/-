d3ef658b2c5bf62e8622e4508605b4df78d64138
王宇乐 <1125532674@qq.com>
2025-11-07 20:43:02 +0800

    Add files via upload

---
+++ 20_数组交集.py
@@ -0,0 +1,14 @@
+from typing import List
+
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        nums1 = set(nums1)
+        nums2 = set(nums2)
+        arr = []
+        i = 0
+        for num in nums1:
+            if num in nums2:
+                arr.append(num)
+                i += 1
+        return arr
+                
